00dd5a5656d60e46ff9de0ee07c79e4a9b989734
Divo123 <topherharsh@gmail.com>
2025-03-17 13:23:25 +0800

    updated PlayerController.java

--- server/src/main/java/com/example/demo/Controllers/PlayerController.java
+++ server/src/main/java/com/example/demo/Controllers/PlayerController.java
@@ -4,7 +4,6 @@ import com.example.demo.Models.Player;
 import com.example.demo.Services.PlayerService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
