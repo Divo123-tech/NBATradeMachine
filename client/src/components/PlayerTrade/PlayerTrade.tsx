9c7dfbc351e3eae866cda51897321a67ed3458c3
Divo123 <topherharsh@gmail.com>
2025-03-19 13:18:42 +0800

    created seperate Player Trade component

---
+++ client/src/components/PlayerTrade/PlayerTrade.tsx
@@ -0,0 +1,62 @@
+import { IoMdInformationCircleOutline } from "react-icons/io";
+import { Player } from "../../services/players.service";
+import { teams } from "../../data/teams";
+type Props = {
+  player: Player;
+};
+
+const PlayerTrade = ({ player }: Props) => {
+  return (
+    <div className="hover:bg-opacity-20 hover:cursor-pointer hover:bg-gray-500">
+      <div
+        key={player.name}
+        className="text-white flex justify-between border-b border-dashed border-gray-500 px-2"
+      >
+        <div className=" py-2 ">
+          <p className="text-lg">{player.name}</p>
+          <div className="flex text-sm text-gray-400 gap-1">
+            <p>{player.position + ", "} </p>
+            <p>{player.age + " yo, "} </p>
+            <p>{player.minutesPlayed} MPG</p>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <div className="border-r-2 border-gray-500 text-center flex-col items-center h-full justify-center py-3 px-1">
+            <p className=" text-md font-semibold">
+              ${(player.season2425 / 1000000).toFixed(1)}M
+            </p>
+            <p className="text-sm font-semibold">{player.contractLength}yrs</p>
+          </div>
+          <div className="border-r border-gray-500 h-full flex-col items-center py-3 px-1">
+            <img src={teams[player.team].logo} className=" w-10 h-10" />
+          </div>
+          <div className="pl-2">
+            <IoMdInformationCircleOutline size={24} />
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-around text-sm text-white py-1 border-b border-gray-500">
+        <p>
+          {player.points} <span className="text-gray-400 text-xs">PTS</span>
+        </p>
+        <p>
+          {player.assists} <span className="text-gray-400 text-xs">AST</span>
+        </p>{" "}
+        <p>
+          {player.trb} <span className="text-gray-400 text-xs">TRB</span>
+        </p>{" "}
+        <p>
+          {player.blocks} <span className="text-gray-400 text-xs">BLK</span>
+        </p>{" "}
+        <p>
+          {player.steals} <span className="text-gray-400 text-xs">STL</span>
+        </p>{" "}
+        <p>
+          {player.turnovers} <span className="text-gray-400 text-xs">TOV</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PlayerTrade;
--- client/src/components/TradeMachine/TradeMachine.tsx
+++ client/src/components/TradeMachine/TradeMachine.tsx
@@ -3,6 +3,7 @@ import { DraftPick } from "../../services/draftpick.service";
 import { Player } from "../../services/players.service";
 import TeamSelect from "./TeamSelect";
 import { teams } from "../../data/teams";
+import PlayerTrade from "../PlayerTrade";
 export type Roster = {
   [team: string]: Player[];
 };
@@ -31,61 +32,7 @@ const TradeMachine = () => {
           {rostersSelected.length > 0 && draftPicksSelected.length > 0 ? (
             <div>
               {rostersSelected[0].map((player: Player) => {
-                return (
-                  <div className="hover:bg-opacity-20 hover:cursor-pointer hover:bg-gray-500">
-                    <div
-                      key={player.name}
-                      className="text-white flex justify-between border-b border-dashed border-gray-500 px-2"
-                    >
-                      <div className=" py-2 ">
-                        <p className="text-lg">{player.name}</p>
-                        <div className="flex text-sm text-gray-400 gap-1">
-                          <p>{player.position + ", "} </p>
-                          <p>{player.age + " yo, "} </p>
-                          <p>{player.minutesPlayed} MPG</p>
-                        </div>
-                      </div>
-                      <div className="flex">
-                        <div className="border-r-2 border-gray-500 px-2 py-2 text-center">
-                          <p className="font-semibold">
-                            ${(player.season2425 / 1000000).toFixed(1)}M
-                          </p>
-                          <p>{player.contractLength}yrs</p>
-                        </div>
-                        <img
-                          src={teams[player.team].logo}
-                          className="object-contain w-14 h-14 px-2  border-gray-500"
-                        />
-                      </div>
-                    </div>
-                    <div className="flex justify-around text-sm text-white py-1 border-b border-gray-500">
-                      <p>
-                        {player.points}{" "}
-                        <span className="text-gray-400 text-xs">PTS</span>
-                      </p>
-                      <p>
-                        {player.assists}{" "}
-                        <span className="text-gray-400 text-xs">AST</span>
-                      </p>{" "}
-                      <p>
-                        {player.trb}{" "}
-                        <span className="text-gray-400 text-xs">TRB</span>
-                      </p>{" "}
-                      <p>
-                        {player.blocks}{" "}
-                        <span className="text-gray-400 text-xs">BLK</span>
-                      </p>{" "}
-                      <p>
-                        {player.steals}{" "}
-                        <span className="text-gray-400 text-xs">STL</span>
-                      </p>{" "}
-                      <p>
-                        {player.turnovers}{" "}
-                        <span className="text-gray-400 text-xs">TOV</span>
-                      </p>
-                    </div>
-                  </div>
-                );
+                return <PlayerTrade player={player} />;
               })}
               {draftPicksSelected[0].map((draftPick: DraftPick, i: number) => {
                 return (
